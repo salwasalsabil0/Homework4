452d0829497b7b3f9e402012723bc692a2f02132
elsadiah <101385293+elsadiah@users.noreply.github.com>
2023-09-20 22:03:52 +0700

    add: transfer.php

--- transfer.php
+++ transfer.php
@@ -1,71 +1,64 @@
 <?php
-header("Content-Type: application/json");
-require_once "config.php";
+// Include file konfigurasi (config.php) untuk koneksi database atau sumber data lainnya
+include "config.php";
 
-// Terima permintaan POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Terima data JSON dari permintaan
-    $requestData = json_decode(file_get_contents("php://input"), true);
+// Cek apakah ada data yang dikirim melalui metode POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Pastikan semua kolom telah diisi
+    if (isset($_POST['id_akun1']) && isset($_POST['id_akun2']) && isset($_POST['jumlah_transaksi'])) {
+        // Ambil nilai dari form
+        $id_akun1 = $_POST['id_akun1'];
+        $id_akun2 = $_POST['id_akun2'];
+        $jumlah_transaksi = $_POST['jumlah_transaksi'];
 
-    // Periksa apakah data yang diperlukan ada
-    if (isset($requestData["from_acc"]) && isset($requestData["to_acc"]) && isset($requestData["amount"])) {
-        $fromAccount = $requestData["from_acc"];
-        $toAccount = $requestData["to_acc"];
-        $amount = floatval($requestData["amount"]);
+        if ($id_akun1 != '' && $id_akun2 != '' && $jumlah_transaksi != '') {
+            // Selalu lakukan sanitasi input sebelum menjalankan query SQL
+            $id_akun1 = intval($id_akun1);
+            $id_akun2 = intval($id_akun2);
+            $jumlah_transaksi = floatval($jumlah_transaksi);
 
-        // Query SQL untuk mengambil saldo pengirim berdasarkan nama akun
-        $sql = "SELECT balance FROM customers WHERE fullname = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $fromAccount);
-        $stmt->execute();
-        $result = $stmt->get_result();
+            // Pastikan akun 1 memiliki saldo yang cukup
+            $query = "SELECT saldo FROM accounts WHERE id_akun = $id_akun1";
+            $result = $conn->query($query);
+            if ($result && $result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $saldo_akun1 = $row['saldo'];
 
-        if ($result->num_rows === 1) {
-            $row = $result->fetch_assoc();
-            $senderBalance = $row["balance"];
+                if ($saldo_akun1 >= $jumlah_transaksi) {
+                    // Mulai transaksi
+                    $conn->begin_transaction();
 
-            // Periksa apakah saldo mencukupi dan lanjutkan dengan transfer jika saldo mencukupi
-            if ($senderBalance >= $amount) {
-                // Mulai transaksi
-                $conn->begin_transaction();
+                    // Update saldo akun 1 dan akun 2 berdasarkan jumlah transaksi
+                    $sql1 = "UPDATE accounts SET saldo = saldo - $jumlah_transaksi WHERE id_akun = $id_akun1";
+                    $sql2 = "UPDATE accounts SET saldo = saldo + $jumlah_transaksi WHERE id_akun = $id_akun2";
 
-                // Update saldo pengirim
-                $sqlUpdateSender = "UPDATE customers SET balance = balance - ? WHERE fullname = ?";
-                $stmtUpdateSender = $conn->prepare($sqlUpdateSender);
-                $stmtUpdateSender->bind_param("ds", $amount, $fromAccount);
-                $stmtUpdateSender->execute();
-
-                // Update saldo penerima
-                $sqlUpdateReceiver = "UPDATE customers SET balance = balance + ? WHERE fullname = ?";
-                $stmtUpdateReceiver = $conn->prepare($sqlUpdateReceiver);
-                $stmtUpdateReceiver->bind_param("ds", $amount, $toAccount);
-                $stmtUpdateReceiver->execute();
-
-                // Catat transaksi
-                $sqlInsertTransaction = "INSERT INTO transactions (from_acc, to_acc, amount) VALUES (?, ?, ?)";
-                $stmtInsertTransaction = $conn->prepare($sqlInsertTransaction);
-                $stmtInsertTransaction->bind_param("ssd", $fromAccount, $toAccount, $amount);
-                $stmtInsertTransaction->execute();
-
-                // Commit transaksi
-                $conn->commit();
-
-                echo json_encode(["message" => "Transfer berhasil"]);
+                    if ($conn->query($sql1) && $conn->query($sql2)) {
+                        // Jika transaksi berhasil, commit dan tampilkan pesan berhasil
+                        $conn->commit();
+                        echo "Transfer dari Akun $id_akun1 ke Akun $id_akun2 sejumlah $jumlah_transaksi berhasil";
+                    } else {
+                        // Jika ada kesalahan dalam query SQL, rollback transaksi
+                        $conn->rollback();
+                        echo "Transfer Gagal: Terjadi kesalahan dalam melakukan transaksi.";
+                    }
+                } else {
+                    // Jika saldo akun 1 tidak mencukupi, tampilkan pesan kesalahan
+                    echo "Transfer Gagal: Saldo Akun $id_akun1 tidak mencukupi.";
+                }
             } else {
-                http_response_code(400);
-                echo json_encode(["message" => "Saldo pengirim tidak mencukupi"]);
+                echo "Transfer Gagal: Akun $id_akun1 tidak ditemukan.";
             }
         } else {
-            http_response_code(404);
-            echo json_encode(["message" => "Akun pengirim tidak ditemukan"]);
+            // Jika ada data yang tidak diisi, tampilkan pesan kesalahan
+            echo "Transfer Gagal: Semua kolom harus diisi.";
         }
     } else {
-        http_response_code(400);
-        echo json_encode(["message" => "Permintaan tidak valid. Pastikan Anda mengirimkan 'from_acc', 'to_acc', dan 'amount' dalam bentuk JSON."]);
+        // Jika tidak ada data POST, tampilkan pesan kesalahan
+        echo "Transfer Gagal: Data yang diperlukan tidak tersedia.";
     }
 } else {
-    http_response_code(405); // Method Not Allowed
-    echo json_encode(["message" => "Metode HTTP tidak diizinkan"]);
+    // Jika bukan metode POST, tampilkan pesan kesalahan
+    echo "Transfer Gagal: Metode yang diperbolehkan hanya POST.";
 }
 
 // Tutup koneksi ke database
